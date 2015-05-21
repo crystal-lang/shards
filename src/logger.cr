294b09d81f7246dd253417872beb77f5af3d92ea
Julien Portalier <julien@portalier.com>
2015-05-21 15:51:44 +0200

    Simple CLI options (verbosity) + logger colors

--- src/logger.cr
+++ src/logger.cr
@@ -1,13 +1,35 @@
 require "logger"
+require "colorize"
 
 module Shards
+  LOGGER_COLORS = {
+    "ERROR" => :red,
+    "WARN"  => :orange,
+    "INFO"  => :light_green,
+    "DEBUG" => :light_gray,
+  }
+
+  @@colors = true
+
+  def self.colors=(value)
+    @@colors = value
+  end
+
   def self.logger
     @@logger ||= Logger.new(STDOUT).tap do |logger|
       logger.progname = "shards"
-      logger.level = Logger::Severity::DEBUG
+      logger.level = Logger::Severity::INFO
 
       logger.formatter = Logger::Formatter.new do |severity, _datetime, _progname, message, io|
-        io << severity[0] << ": " << message
+        if @@colors
+          io << if color = LOGGER_COLORS[severity.to_s]?
+                  message.colorize(color)
+                else
+                  message
+                end
+        else
+          io << severity[0] << ": " << message
+        end
       end
     end
   end
--- src/shards.cr
+++ src/shards.cr
@@ -8,16 +8,43 @@ module Shards
   Dir.mkdir(INSTALL_PATH) unless Dir.exists?(INSTALL_PATH)
 end
 
+require "option_parser"
 require "./logger"
 require "./errors"
 require "./spec"
 require "./manager"
 
 begin
+  OptionParser.parse! do |opts|
+    opts.banner = "shards [options]"
+
+    opts.on("--no-colors", "") do
+      Shards.colors = false
+    end
+
+    opts.on("-V", "--verbose", "") do
+      Shards.logger.level = Logger::Severity::DEBUG
+    end
+
+    opts.on("-q", "--quiet", "") do
+      Shards.logger.level = Logger::Severity::WARN
+    end
+
+    opts.on("-h", "--help", "") do
+      puts opts
+      exit
+    end
+  end
+
   spec = Shards::Spec.from_file(Dir.working_directory)
   manager = Shards::Manager.new(spec)
   manager.resolve
   manager.packages.each(&.install)
+
+rescue ex : OptionParser::InvalidOption
+  Shards.logger.fatal ex.message
+  exit -1
+
 rescue ex : Shards::Error
   Shards.logger.error ex.message
   exit -1
