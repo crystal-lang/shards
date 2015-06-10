crystal-lang/shards

commit adefcf3a725ec77961154510b1a4cb6c8ac96cbc

Fix: Clone Git repository again when remote changes
fixes #4

src/resolvers/git.cr
@@ -63,7 +63,7 @@ module Shards
     end
 
     def git_url
-      dependency["git"].to_s
+      dependency["git"].to_s.strip
     end
 
     private def git_refs(version)
@@ -88,6 +88,11 @@ module Shards
     end
 
     private def update_local_cache
+      if origin_changed?
+        delete_repository
+        @updated_cache = false
+      end
+
       return if !@update_cache || @updated_cache
       Shards.logger.info "Updating #{git_url}"
 
@@ -113,8 +118,16 @@ module Shards
       raise Error.new("Failed to update #{git_url}")
     end
 
+    private def delete_repository
+      run "rm -rf #{local_path}"
+    end
+
     private def cloned_repository?
       Dir.exists?(local_path)
+    end
+
+    private def origin_changed?
+      capture("git ls-remote --get-url origin").strip != git_url
     end
 
     private def file_exists?(refs, path)

commit 26e082abf3c881b87fac6207c2a9097d7f9e0dec

Fix: can't clone new dependency
Fixed a regression that occured after adefcf3 which checks if the
origin remote changed, but didn't verify if is had been cloned already.

src/resolvers/git.cr
@@ -88,7 +88,7 @@ module Shards
     end
 
     private def update_local_cache
-      if origin_changed?
+      if cloned_repository? && origin_changed?
         delete_repository
         @updated_cache = false
       end