6da2f0260a7dec8a7530a2f8b8a34544fe1739e0
Julien Portalier <julien@portalier.com>
2015-09-28 11:28:24 +0200

    Add git commit sha1 to shards --version
    closes #53

--- src/version.cr
+++ src/version.cr
@@ -1,8 +1,13 @@
 module Shards
   VERSION = {{ `cat VERSION`.stringify.chomp }}
+  BUILD_SHA1 = {{ `git log --format=%h -n 1 2>/dev/null || echo ""`.stringify.chomp }}
   BUILD_DATE = {{ `date -u +'%Y-%m-%d'`.stringify.chomp }}
 
   def self.version_string
-    "Shards #{VERSION} (#{BUILD_DATE})"
+    if BUILD_SHA1.empty?
+      "Shards #{VERSION} (#{BUILD_DATE})"
+    else
+      "Shards #{VERSION} [#{BUILD_SHA1}] (#{BUILD_DATE})"
+    end
   end
 end
