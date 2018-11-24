f7bb47e896bbf7ed4b7e79d929b19efe96583752
Arnaud Berthomier <oz@users.noreply.github.com>
2018-11-24 09:57:57 -0600

    Add --tree option to shards list (#237)
    * Add --tree option to shards list
    
    * Do not pass command name argument to list
    
    * Remove useless self
    
    * Test for list --tree
    
    * Fix that out-of-bound error
    
    * Move options parsing out of Command::List
    
    * Update help message for list command

--- src/commands/list.cr
+++ src/commands/list.cr
@@ -4,15 +4,18 @@ require "./command"
 module Shards
   module Commands
     class List < Command
+      def self.run(path, @@tree = false)
+        super
+      end
+
       def run(*args)
         return unless has_dependencies?
-
         puts "Shards installed:"
         list(spec.dependencies)
         list(spec.development_dependencies) unless Shards.production?
       end
 
-      private def list(dependencies)
+      private def list(dependencies, level = 1)
         dependencies.each do |dependency|
           resolver = Shards.find_resolver(dependency)
 
@@ -22,9 +25,11 @@ module Shards
             raise Error.new("Dependencies aren't satisfied. Install them with 'shards install'")
           end
 
-          puts "  * #{_spec.name} (#{_spec.version})"
+          indent = "  " * level
+          puts "#{indent}* #{_spec.name} (#{_spec.version})"
 
-          list(_spec.dependencies)
+          indent_level = @@tree ? level + 1 : level
+          list(_spec.dependencies, indent_level)
         end
       end
 
