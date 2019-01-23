6a8c9bf5ca02791543185c3854871eb5db47563f
Julien Portalier <julien@portalier.com>
2019-01-23 15:51:48 +0100

    fixup: cleanup lock command a little

--- src/commands/lock.cr
+++ src/commands/lock.cr
@@ -17,43 +17,29 @@ module Shards
         end
 
         Shards.logger.info do
-          total = pkgs.reduce(0) { |acc, (name, pkg)| acc + pkg.versions.size }
+          total = pkgs.reduce(0) { |acc, (_, pkg)| acc + pkg.versions.size }
           "Collected #{pkgs.size} dependencies (total: #{total} specs, duration: #{spent})"
         end
-
-        sat = Shards::SAT.new
 
         Shards.logger.info { "Building SAT clauses..." }
 
         spent = Time.measure do
-          negation = "~#{spec.name}"
-
           # the package to install dependencies for:
           sat.add_clause [spec.name]
 
           # main dependencies:
-          spec.dependencies.each do |d|
-            clause = [negation]
-            Versions
-              .resolve(pkgs[d.name].versions.keys, {d.version})
-              .each { |v| clause << "#{d.name}:#{v}" }
-            sat.add_clause(clause)
-          end
+          negation = "~#{spec.name}"
+          add_dependencies(negation, spec.dependencies)
+          add_dependencies(negation, spec.development_dependencies) unless Shards.production?
 
           # nested dependencies:
           pkgs.each do |name, pkg|
             pkg.versions.each do |version, s|
-              s.dependencies.each do |d|
-                clause = ["~#{name}:#{version}"]
-                Versions
-                  .resolve(pkgs[d.name].versions.keys, {d.version})
-                  .map { |v| clause << "#{d.name}:#{v}" }
-                sat.add_clause(clause) unless clause.size == 1
-              end
+              add_dependencies("~#{name}:#{version}", s.dependencies)
             end
           end
 
-          # version conflicts (only 1 version per package)
+          # version conflicts (we want only 1 version per package):
           pkgs.each do |name, pkg|
             pkg.versions.keys.each_combination(2) do |(a, b)|
               sat.add_clause ["~#{name}:#{a}", "~#{name}:#{b}"]
@@ -62,9 +48,9 @@ module Shards
         end
         Shards.logger.info { "Built #{sat.@clauses.size} clauses (duration: #{spent})" }
 
-        sat.@clauses.each do |clause|
-          STDERR.puts sat.clause_to_s(clause)
-        end
+        # sat.@clauses.each do |clause|
+        #   STDERR.puts sat.clause_to_s(clause)
+        # end
 
         Shards.logger.info { "Solving dependencies..." }
         count = 0
@@ -73,10 +59,8 @@ module Shards
           sat.solve do |solution|
             # p solution
             count += 1
-            print "found: #{count} solutions\r" if count == 100
           end
         end
-        print "found: #{count} solutions\n"
 
         if count == 0
           Shards.logger.error { "Failed to find a solution (duration: #{spent})" }
@@ -85,15 +69,29 @@ module Shards
         end
       end
 
-      protected def sat
+      private def sat
         @sat ||= SAT.new
       end
 
-      protected def pkgs
+      private def add_dependencies(negation, dependencies)
+        dependencies.each do |d|
+          versions = Versions.resolve(pkgs[d.name].versions.keys, {d.version})
+
+          # FIXME: looks like we couldn't resolve a constraint here; maybe it's
+          # related to a git refs, or something?
+          next if versions.empty?
+
+          clause = [negation]
+          versions.each { |v| clause << "#{d.name}:#{v}" }
+          sat.add_clause(clause)
+        end
+      end
+
+      private def pkgs
         @pkgs ||= {} of String => Pkg
       end
 
-      protected def dig(dependencies, resolve = false)
+      private def dig(dependencies, resolve = false)
         dependencies.each do |dependency|
           next if pkgs.has_key?(dependency.name)
 
@@ -109,7 +107,6 @@ module Shards
           pkg.versions.each do |version, spec|
             next unless version =~ VERSION_REFERENCE
             dig(spec.dependencies)
-            dig(spec.development_dependencies) unless Shards.production?
           end
         end
       end
