0f76214ccbc4b53d5b0a5b8c0f7a64ff3de8fa27
Brian J. Cardiff <bcardiff@gmail.com>
2020-03-20 16:16:21 -0300

    Port git resolver

--- spec/git_resolver_spec.cr
+++ spec/git_resolver_spec.cr
@@ -1,8 +1,14 @@
-require "./test_helper"
+require "./spec_helper"
+
+private def resolver(name, config = {} of String => String)
+  config = config.merge({"git" => git_url(name)})
+  dependency = Shards::Dependency.from_name_config(name, config)
+  Shards::GitResolver.new(dependency)
+end
 
 module Shards
-  class GitResolverTest < Minitest::Test
-    def setup
+  describe PathResolver do
+    before_each do
       create_git_repository "empty"
       create_git_commit "empty", "initial release"
 
@@ -13,32 +19,32 @@ module Shards
       create_git_tag "library", "99.9.9"
     end
 
-    def test_available_versions
-      assert_equal ["HEAD"], resolver("empty").available_versions
-      assert_equal ["0.0.1", "0.1.0", "0.1.1", "0.1.2", "0.2.0"], resolver("library").available_versions
+    it "available versions" do
+      resolver("empty").available_versions.should eq(["HEAD"])
+      resolver("library").available_versions.should eq(["0.0.1", "0.1.0", "0.1.1", "0.1.2", "0.2.0"])
     end
 
-    def test_read_spec
-      assert_raises { resolver("empty").read_spec }
-      assert_equal "name: library\nversion: 0.2.0\nauthors:\n  - julien <julien@portalier.com>", resolver("library").read_spec
-      assert_equal "name: library\nversion: 0.1.1\n", resolver("library").read_spec("0.1.1")
-      assert_equal "name: library\nversion: 0.1.1\n", resolver("library").read_spec("0.1.1")
+    it "read spec" do
+      expect_raises(Error) { resolver("empty").read_spec }
+      resolver("library").read_spec.should eq("name: library\nversion: 0.2.0\nauthors:\n  - julien <julien@portalier.com>")
+      resolver("library").read_spec("0.1.1").should eq("name: library\nversion: 0.1.1\n")
+      resolver("library").read_spec("0.1.1").should eq("name: library\nversion: 0.1.1\n")
     end
 
-    def test_install
+    it "install" do
       library = resolver("library")
 
       library.install("0.1.2")
-      assert File.exists?(install_path("library", "src/library.cr"))
-      assert File.exists?(install_path("library", "shard.yml"))
-      assert_equal "0.1.2", library.installed_spec.not_nil!.version
-      # assert File.exists?(install_path("library", "LICENSE"))
+      File.exists?(install_path("library", "src/library.cr")).should be_true
+      File.exists?(install_path("library", "shard.yml")).should be_true
+      library.installed_spec.not_nil!.version.should eq("0.1.2")
+      # File.exists?(install_path("library", "LICENSE")).should be_true
 
       library.install
-      assert_equal "0.2.0", library.installed_spec.not_nil!.version
+      library.installed_spec.not_nil!.version.should eq("0.2.0")
     end
 
-    def test_origin_changed
+    it "origin changed" do
       dependency = Dependency.new("library", {"git" => git_url("library")})
       library = GitResolver.new(dependency)
       library.install("0.1.2")
@@ -63,7 +69,7 @@ module Shards
 
       same_origins.each do |origin|
         dependency["git"] = origin
-        refute library.origin_changed?, origin
+        library.origin_changed?.should be_false
       end
 
       # These alternatives should all trigger origin as changed
@@ -79,20 +85,14 @@ module Shards
 
       changed_origins.each do |origin|
         dependency["git"] = origin
-        assert library.origin_changed?, origin
+        library.origin_changed?.should be_true
       end
     end
 
-    def test_install_refs
-      skip "TODO: install commit (whatever the version)"
-      skip "TODO: install branch (whatever the version)"
-      skip "TODO: install tag (whatever the version)"
-    end
-
-    private def resolver(name, config = {} of String => String)
-      config = config.merge({"git" => git_url(name)})
-      dependency = Dependency.new(name, config)
-      GitResolver.new(dependency)
+    pending "install refs" do
+      # TODO: install commit (whatever the version)
+      # TODO: install branch (whatever the version)
+      # TODO: install tag (whatever the version)
     end
   end
 end
