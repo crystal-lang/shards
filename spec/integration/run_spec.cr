ac07c952d74681ced91e2eb70cce26412de14dea
Elliot DeNolf <denolfe@gmail.com>
2020-03-23 23:09:09 -0400

    Update to use std-lib spec instead of minitest.cr

--- spec/integration/run_spec.cr
+++ spec/integration/run_spec.cr
@@ -1,20 +1,20 @@
-require "../integration_helper"
+require "./spec_helper"
 
-class RunCommandTest < Minitest::Test
-  def setup
+def bin_path(name)
+  File.join(application_path, "bin", name)
+end
+
+describe "run" do
+  before_each do
     Dir.mkdir(File.join(application_path, "src"))
     File.write(File.join(application_path, "src", "cli.cr"), "puts __FILE__")
   end
 
-  def teardown
+  after_each do
     File.delete File.join(application_path, "shard.yml")
   end
 
-  def bin_path(name)
-    File.join(application_path, "bin", name)
-  end
-
-  def test_runs_specific_target
+  it "runs specific target" do
     File.write File.join(application_path, "shard.yml"), <<-YAML
       name: build
       version: 0.1.0
@@ -27,13 +27,12 @@ class RunCommandTest < Minitest::Test
 
     Dir.cd(application_path) do
       run "shards run app --no-color"
-      assert File.exists?(bin_path("app"))
-
-      assert_equal File.join(application_path, "src", "cli.cr"), `#{bin_path("app")}`.chomp
+      File.exists?(bin_path("app")).should be_true
+      `#{bin_path("app")}`.chomp.should eq(File.join(application_path, "src", "cli.cr"))
     end
   end
 
-  def test_runs_when_only_one_target
+  it "runs when only one target" do
     File.write File.join(application_path, "shard.yml"), <<-YAML
       name: build
       version: 0.1.0
@@ -44,13 +43,12 @@ class RunCommandTest < Minitest::Test
 
     Dir.cd(application_path) do
       run "shards run --no-color"
-      assert File.exists?(bin_path("app"))
-
-      assert_equal File.join(application_path, "src", "cli.cr"), `#{bin_path("app")}`.chomp
+      File.exists?(bin_path("app")).should be_true
+      `#{bin_path("app")}`.chomp.should eq(File.join(application_path, "src", "cli.cr"))
     end
   end
 
-  def test_fails_when_multiple_targets_no_arg
+  it "fails when multiple targets, no arg" do
     File.write File.join(application_path, "shard.yml"), <<-YAML
       name: build
       version: 0.1.0
@@ -62,42 +60,42 @@ class RunCommandTest < Minitest::Test
       YAML
 
     Dir.cd(application_path) do
-      ex = assert_raises(FailedCommand) do
-        run "shards run --no-color", true
-      end
-      assert_match /Error please specify the target with 'shards run target'/, ex.stdout
-    end
-  end
-
-  def test_fails_when_no_targets_defined
-    File.write File.join(application_path, "shard.yml"), <<-YAML
-      name: build
-      version: 0.1.0
-      YAML
-
-    Dir.cd(application_path) do
-      ex = assert_raises(FailedCommand) do
+      ex = expect_raises(FailedCommand) do
         run "shards run --no-color"
       end
-      assert_match /Error no targets defined/, ex.stdout
+      ex.stdout.should contain("Error please specify the target with 'shards run target'")
     end
   end
 
-  def test_fails_when_no_targets_defined_with_target
+  it "fails when no targets defined" do
     File.write File.join(application_path, "shard.yml"), <<-YAML
       name: build
       version: 0.1.0
       YAML
 
     Dir.cd(application_path) do
-      ex = assert_raises(FailedCommand) do
-        run "shards run missing --no-color"
+      ex = expect_raises(FailedCommand) do
+        run "shards run --no-color"
       end
-      assert_match /Error target missing was not found in/, ex.stdout
+      ex.stdout.should contain("Error no targets defined")
     end
   end
 
-  def test_fails_when_passing_multiple_targets
+  it "fails when no targets defined with target" do
+    File.write File.join(application_path, "shard.yml"), <<-YAML
+      name: build
+      version: 0.1.0
+      YAML
+
+    Dir.cd(application_path) do
+      ex = expect_raises(FailedCommand) do
+        run "shards run missing --no-color"
+      end
+      ex.stdout.should contain("Error target missing was not found in")
+    end
+  end
+
+  it "fails when passing multiple targets" do
     File.write File.join(application_path, "shard.yml"), <<-YAML
       name: build
       version: 0.1.0
@@ -109,10 +107,10 @@ class RunCommandTest < Minitest::Test
       YAML
 
     Dir.cd(application_path) do
-      ex = assert_raises(FailedCommand) do
+      ex = expect_raises(FailedCommand) do
         run "shards run app alt --no-color"
       end
-      assert_match /Error please specify only one target. If you meant to pass arguments you may use 'shards run target -- args'/, ex.stdout
+      ex.stdout.should contain("Error please specify only one target. If you meant to pass arguments you may use 'shards run target -- args")
     end
   end
 end
