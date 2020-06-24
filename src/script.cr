8f968f6bd84dfc7ae9340eefde2de3ec5e02e255
Brian J. Cardiff <bcardiff@gmail.com>
2020-06-24 17:55:35 -0300

    Create a temp directory with symlinks to shards and crystal

--- src/script.cr
+++ src/script.cr
@@ -6,9 +6,47 @@ module Shards
     def self.run(path, command, script_name, dependency_name)
       Dir.cd(path) do
         output = IO::Memory.new
-        status = Process.run("/bin/sh", input: IO::Memory.new(command), output: output, error: output)
+        status = Process.run("/bin/sh", env: Script.environment, input: IO::Memory.new(command), output: output, error: output)
         raise Error.new("Failed #{script_name} of #{dependency_name} on #{command}:\n#{output.to_s.rstrip}") unless status.success?
       end
+    end
+
+    @@environment : Process::Env = nil
+
+    def self.environment : Process::Env
+      @@environment ||=
+        begin
+          bin_path = File.tempname(".shards-env")
+          Dir.mkdir(bin_path)
+          at_exit do
+            Dir.new(bin_path).each_child do |e|
+              File.delete(File.join(bin_path, e))
+            end
+            Dir.delete bin_path
+          end
+          env_path = prepend_path(bin_path, ENV["PATH"]?)
+          Log.debug { "Creating shards script environment created at #{bin_path}. PATH=#{env_path}" }
+
+          add_executable bin_path, "shards", Process.executable_path
+          add_executable bin_path, "crystal", Process.find_executable(Shards.crystal_bin)
+
+          {"PATH" => env_path}
+        rescue e
+          Log.error(exception: e) { "Unable to create shards script environment" }
+
+          {} of String => String
+        end
+    end
+
+    private def self.add_executable(bin_path : String, name : String, original_path : String?)
+      if original_path
+        Log.debug { "Adding #{name}=#{original_path} to shards script environment" }
+        File.symlink(original_path, File.join(bin_path, name))
+      end
+    end
+
+    private def self.prepend_path(prefix : String, suffix : String?)
+      suffix ? "#{prefix}:#{suffix}" : prefix
     end
   end
 end
