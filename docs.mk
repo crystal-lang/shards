b0eb36325ac68ea81a57869d6deb74c70840334a
Johannes Müller <straightshoota@gmail.com>
2020-11-13 12:05:03 +0100

    Transfer content from SPEC.md to docs/shard.yml.adoc

--- docs.mk
+++ docs.mk
@@ -1,13 +1,14 @@
 ASCIIDOC ?= asciidoctor
 
-MAN_FILES := man/shards.1 man/shard.yml.5
+ASCIIDOC_OPTIONS = -a year=$(shell date +%Y) -a shards_version=$(SHARDS_VERSION)
 
+MAN_FILES := man/shards.1 man/shard.yml.5
 docs: manpages
 
 manpages: $(MAN_FILES)
 
 man/%.1 man/%.5: docs/%.adoc
-	$(ASCIIDOC) $< -b manpage -o $@ -a year=$(shell date +%Y) -a shards_version=$(SHARDS_VERSION)
+	$(ASCIIDOC) $(ASCIIDOC_OPTIONS) $< -b html5 -o $@
 
 clean_docs: phony
 	rm -f $(MAN_FILES)
