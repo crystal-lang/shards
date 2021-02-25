da392b8b08adc71efa544c9f5dc83d9f7f17a615
Johannes Müller <straightshoota@gmail.com>
2021-02-25 01:49:04 +0100

    Fix broken SOURCE_DATE_EPOCH in docs.mk

--- docs.mk
+++ docs.mk
@@ -6,7 +6,7 @@ MAN_FILES := man/shards.1 man/shard.yml.5
 HTML_FILES := docs/shards.html docs/shard.yml.html
 
 SHARDS_VERSION := $(shell cat VERSION)
-source_date_epoch := $(shell (git show -s --format=%ct HEAD || stat -c "%Y" $(1) || stat -f "%m" $(1)) 2> /dev/null)
+SOURCE_DATE_EPOCH := $(shell (git show -s --format=%ct HEAD || stat -c "%Y" Makefile || stat -f "%m" Makefile) 2> /dev/null)
 
 docs: manpages
 
@@ -15,10 +15,10 @@ manpages: $(MAN_FILES)
 htmlpages: $(HTML_FILES)
 
 man/%.1 man/%.5: docs/%.adoc
-	SOURCE_DATE_EPOCH=$(call source_date_epoch,$<) $(ASCIIDOC) $(ASCIIDOC_OPTIONS) $< -b manpage -o $@
+	SOURCE_DATE_EPOCH=$(SOURCE_DATE_EPOCH) $(ASCIIDOC) $(ASCIIDOC_OPTIONS) $< -b manpage -o $@
 
 docs/%.html: docs/%.adoc
-	SOURCE_DATE_EPOCH=$(call source_date_epoch,$<) $(ASCIIDOC) $(ASCIIDOC_OPTIONS) $< -b html5 -o $@
+	SOURCE_DATE_EPOCH=$(SOURCE_DATE_EPOCH) $(ASCIIDOC) $(ASCIIDOC_OPTIONS) $< -b html5 -o $@
 
 clean_docs: phony
 	rm -f $(MAN_FILES)
