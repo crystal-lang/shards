49ac7671e0e335f94eedb2f4ec22183688c1f7cd
Luis Lavena <luislavena@gmail.com>
2024-05-20 22:53:28 +0200

    Only use dummy executable on Windows
    Follow the pattern used in other tests and use simple `sh` script on
    non-Windows platforms.

--- spec/integration/subcommand_spec.cr
+++ spec/integration/subcommand_spec.cr
@@ -3,21 +3,31 @@ require "./spec_helper"
 describe "subcommand" do
   it "forwards all arguments to subcommand" do
     create_shard("dummy", "0.1.0")
-    create_executable "dummy", "bin/shards-dummy", %(print "args: "; print ARGV)
+    {% if flag?(:win32) %}
+      create_executable "dummy", "bin/shards-dummy", %(print ARGV.join(" "))
+    {% else %}
+      path = create_file("dummy", "bin/shards-dummy", "#!/bin/sh\necho $@\n")
+      File.chmod(path, 0o755)
+    {% end %}
 
     with_path(git_path("dummy/bin")) do
       output = run("shards dummy --no-color --verbose --unknown other argument")
-      output.should contain(%(args: ["--no-color", "--verbose", "--unknown", "other", "argument"]))
+      output.should contain(%(--no-color --verbose --unknown other argument))
     end
   end
 
   it "correctly forwards '--help' option to subcommand" do
     create_shard("dummy", "0.1.0")
-    create_executable "dummy", "bin/shards-dummy", %(print "args: "; print ARGV)
+    {% if flag?(:win32) %}
+      create_executable "dummy", "bin/shards-dummy", %(print ARGV.join(" "))
+    {% else %}
+      path = create_file("dummy", "bin/shards-dummy", "#!/bin/sh\necho $@\n")
+      File.chmod(path, 0o755)
+    {% end %}
 
     with_path(git_path("dummy/bin")) do
       output = run("shards dummy --help")
-      output.should contain(%(args: ["--help"]))
+      output.should contain(%(--help))
     end
   end
 end
