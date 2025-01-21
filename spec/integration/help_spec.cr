b72eacf95104cd6ca99221e2206284851156a1c3
Julien Portalier <julien@portalier.com>
2025-01-21 12:21:59 +0100

    Fix `--help` option (#665)
    Restores the behavior where `--help` would always skip the command to be invoked, while still allowing to pass the `--help` to external subcommands:
    
    The following commands print the help message and exit:
    
    - `shards --help`
    - `shards --local --help`
    - `shards update --help`
    
    While the following tries to call `shards-unknown --help` then falls back to print the help message and exit:
    
    - `shards unknown --help` 
    
    Relies on a fixed list of the builtin command names to avoid having each `when` case starting with `display_help_and_exit(opts) if display_help`.

---
+++ spec/integration/help_spec.cr
@@ -0,0 +1,29 @@
+require "./spec_helper"
+
+describe "--help" do
+  it "prints help and doesn't invoke the command" do
+    metadata = {
+      version:      "1.0.0",
+      dependencies: {
+        mock: {git: git_path("mock")},
+      },
+    }
+
+    [
+      "shards --help",
+      "shards --local --help",
+      "shards update --help",
+    ].each do |command|
+      with_shard(metadata) do
+        output = run command
+
+        # it printed the help message
+        output.should contain("Commands:")
+        output.should contain("General options:")
+
+        # it didn't run the command (or default command)
+        output.should_not contain("Resolving dependencies")
+      end
+    end
+  end
+end
