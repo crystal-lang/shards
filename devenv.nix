e541036cfdc50f53463fafdc1e07848113075ae4
Johannes Müller <straightshoota@gmail.com>
2025-12-23 12:22:25 +0100

    Enable various hooks

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,8 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
+  dotenv.enable = true;
+
   packages = with pkgs; [
     # build deps
     gnumake
@@ -20,7 +22,21 @@
   };
 
   git-hooks.hooks = {
-    shellcheck.enable = true;
+    check-toml.enable = true;
+    check-vcs-permalinks.enable = true;
     crystal.enable = true;
+    makefile_both = {
+      enable = true;
+      name = "Change both Makefile and Makefile.win";
+      entry = ''${pkgs.runtimeShell} -c 'test "$#" -ne 1 || (echo "Changes only in $@" && false)' --'';
+      files = "^Makefile(\.win)?$";
+      pass_filenames = true;
+    };
+    shellcheck = {
+      enable = true;
+      excludes = [
+        ".*\.zsh$"
+      ];
+    };
   };
 }
